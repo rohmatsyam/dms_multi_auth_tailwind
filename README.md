24807b554db22c30ca42101428737c1a623c4966
rohmatsyam <rohmatsyam@yahoo.com>
2022-05-27 17:12:46 +0700

    "inisialisasi project"

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+
+
+# Laravel 9 Multi Auth System using laravel/breeze package
+
+## Laravel9 + Tailwind Css with Multi-Auth (Admin and User)
+- Admin Login
+- User Login and Register
+
+
+## How to run the code
+- git clone
+- cd project
+- cp .env.example `.env`
+- open .env and update DB_DATABASE (database details)
+- run : `composer install`
+- run : `php artisan key:generate`
+- run : `php artisan migrate:fresh --seed`
+- run : `php artisan serve`
+
+- Best of luck 
+
+
+## Credentials
+- #### Admin
+- email: admin@admin.com
+- password : password
+- #### user
+- email: user@user.com
+- password: password
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => 'auth' ],function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::view('profile','profile')->name('profile');
+    Route::put('profile',[\App\Http\Controllers\ProfileController::class,'update'])->name('profile.update');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin'])->name('admin.dashboard');
+
+require __DIR__.'/adminauth.php';
+
+
+
